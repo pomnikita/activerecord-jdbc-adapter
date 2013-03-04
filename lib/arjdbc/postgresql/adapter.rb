28637bb3f815f615c82e1a0ad5536d1524e8ef7a
Alexey Noskov <alexey.noskov@gmail.com>
2013-03-04 16:32:49 +0400

    Sync native pg type map

--- lib/arjdbc/postgresql/adapter.rb
+++ lib/arjdbc/postgresql/adapter.rb
@@ -190,20 +190,33 @@ module ::ArJdbc
     ADAPTER_NAME = 'PostgreSQL'
 
     NATIVE_DATABASE_TYPES = {
-      :primary_key => "serial primary key",
-      :string      => { :name => "character varying", :limit => 255 },
-      :text        => { :name => "text" },
-      :integer     => { :name => "integer" },
-      :float       => { :name => "float" },
-      :decimal     => { :name => "decimal" },
-      :datetime    => { :name => "timestamp" },
-      :timestamp   => { :name => "timestamp" },
-      :time        => { :name => "time" },
-      :date        => { :name => "date" },
-      :binary      => { :name => "bytea" },
-      :boolean     => { :name => "boolean" },
-      :xml         => { :name => "xml" },
-      :tsvector    => { :name => "tsvector" }
+      primary_key: "serial primary key",
+      string:      { name: "character varying", limit: 255 },
+      text:        { name: "text" },
+      integer:     { name: "integer" },
+      float:       { name: "float" },
+      decimal:     { name: "decimal" },
+      datetime:    { name: "timestamp" },
+      timestamp:   { name: "timestamp" },
+      time:        { name: "time" },
+      date:        { name: "date" },
+      daterange:   { name: "daterange" },
+      numrange:    { name: "numrange" },
+      tsrange:     { name: "tsrange" },
+      tstzrange:   { name: "tstzrange" },
+      int4range:   { name: "int4range" },
+      int8range:   { name: "int8range" },
+      binary:      { name: "bytea" },
+      boolean:     { name: "boolean" },
+      xml:         { name: "xml" },
+      tsvector:    { name: "tsvector" },
+      hstore:      { name: "hstore" },
+      inet:        { name: "inet" },
+      cidr:        { name: "cidr" },
+      macaddr:     { name: "macaddr" },
+      uuid:        { name: "uuid" },
+      json:        { name: "json" },
+      ltree:       { name: "ltree" }
     }
 
     def adapter_name #:nodoc:
